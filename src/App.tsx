1eff959b443c3b3587d3b4d635c6bf9e275b1bbf
CarlosWirama <carloswirama@gmail.com>
2021-02-14 12:58:23 +0800

    replaced samples with real component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GoogleMapReact from 'google-map-react';
 
+function Marker({ lat, lng }: { lat: number; lng: number }) {
+  // function Marker() {
+  return (<div style={{ background: 'white', minWidth: 60 }}>qwerty</div>)
+}
 function App() {
+  const center = {
+    lat: 51.5049375,
+    lng: -0.0964509,
+  };
+  const zoom = 11;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Important message from plugin: Always set the container height explicitly
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        // bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+        defaultCenter={center}
+        defaultZoom={zoom}
+      >
+        <Marker
+          lat={center.lat}
+          lng={center.lng}
+        />
+      </GoogleMapReact>
     </div>
   );
 }
